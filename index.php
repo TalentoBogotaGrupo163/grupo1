a2f5f252d46147ed55ae1159586b331e66f51be9
Andres Castellanos <camilosama64@gmail.com>
2018-09-04 15:34:19 -0500

    Cambio 2

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	echo "HOLA MUNDO 2 <br>";
+	$conn = mysqli_connect('127.0.0.1','root','','bd');
+	$sql = "SELECT id FROM datos";
+	$result = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id:".$row["id"]."<br>";
+    }
+?>
